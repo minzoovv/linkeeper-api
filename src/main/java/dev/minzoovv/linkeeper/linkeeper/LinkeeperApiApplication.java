1d4688d4a0551517b0193d7038558c408dc20184
minzoovv <kmju1997@gmail.com>
2021-05-17 00:53:13 +0900

    init project

---
+++ src/main/java/dev/minzoovv/linkeeper/linkeeper/LinkeeperApiApplication.java
@@ -0,0 +1,13 @@
+package dev.minzoovv.linkeeper.linkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkeeperApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkeeperApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/minzoovv/linkeeper/linkeeper/LinkeeperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.minzoovv.linkeeper.linkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkeeperApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
